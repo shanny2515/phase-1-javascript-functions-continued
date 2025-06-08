47390bde04255e36b80f3a09c0cb4efa97dc57ce
shanny2515 <damarshania05@gmail.com>
2025-06-08 19:09:08 +0300

    modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+   return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun('bathe my dog');
+
+function mondayWork(act = "go to the office"){
+    return (`This Monday, I will ${act}.`);
+}
+mondayWork('work from home');
+
+ function wrapAdjective( flair ="*"){
+    return function(you = "special"){
+        return (`You are ${flair}${you}${flair}!`)
+    }
+ }
+ console.log(wrapAdjective('*')('a hard worker'));
+ console.log(wrapAdjective('||')('a dedicated programmer'));
